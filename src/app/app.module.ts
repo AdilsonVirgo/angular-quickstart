01042b50a93bdc516bfadd13806bacd1b278e52c
Adilson <adilsonvirgo@gmail.com>
2022-06-24 01:10:56 -0500

    creation say hello and photo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { PhotographyComponent } from './photography/photography.component';
+import { SayhelloComponent } from './sayhello/sayhello.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    PhotographyComponent,
+    SayhelloComponent
   ],
   imports: [
     BrowserModule,
