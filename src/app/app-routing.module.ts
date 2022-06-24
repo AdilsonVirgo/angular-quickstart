210101042f0ec1c395246a669e136a7e9aaef052
Adilson <adilsonvirgo@gmail.com>
2022-06-24 01:17:14 -0500

    adding photography component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
+import { PhotographyComponent } from './photography/photography.component';
 
 
 const routes: Routes = [
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'photography', component: PhotographyComponent },  
 ];
 
 @NgModule({
