8d4dc67254397dc4fe85577c956d62ac09f431b0
Adilson <adilsonvirgo@gmail.com>
2022-06-24 01:19:17 -0500

    sayhello routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
 import { PhotographyComponent } from './photography/photography.component';
+import { SayhelloComponent } from './sayhello/sayhello.component';
 
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'photography', component: PhotographyComponent },  
+  { path: 'sayhello', component: SayhelloComponent },  
 ];
 
 @NgModule({
